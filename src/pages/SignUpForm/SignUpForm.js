d03403062ea21102479fc222f7019d9b4a17c153
ERGarima <42827337+ERGarima@users.noreply.github.com>
2019-08-09 12:33:16 +0530

    modify css

--- src/pages/SignUpForm/SignUpForm.js
+++ src/pages/SignUpForm/SignUpForm.js
@@ -298,9 +298,11 @@ class SignUpForm extends Component {
                         <NavLink to="/sign-in" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign In</NavLink>
                         <NavLink exact to="/" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign Up</NavLink>
                     </div>
-                    <div className="FormTitle">
-                        <NavLink to="/sign-in" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign In</NavLink> or <NavLink exact to="/" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign Up</NavLink>
-                    </div>
+                    {/* <div className="FormTitle">
+                        <NavLink to="/sign-in" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign In</NavLink>
+                         or 
+                        <NavLink exact to="/" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign Up</NavLink>
+                    </div> */}
                     {/* <div className="card col-lg-11 col-xl-11 panel-body "> */}
                     <div className="col-xs-10 col-sm-10 col-md-10 col-lg-10">
                         <div className="FormCenter">
