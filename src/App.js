2d0d403bee19f7b6e1bb1fdc632e2cf3a4566de2
ERGarima <42827337+ERGarima@users.noreply.github.com>
2019-08-09 11:46:30 +0530

    registration form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { HashRouter as Router, Route, Switch, Link, NavLink } from 'react-router-dom';
+import SignUpForm from './pages/SignUpForm/SignUpForm';
+import SignInForm from './pages/SignInForm/SignInForm';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router basename="/hcl-myproduct/">
+        <Switch>
+          <Route exact path="/" component={SignUpForm} />
+          {/* <Route exact path="/" component={SignInForm} /> */}
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
